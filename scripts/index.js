c13f9ad72f05fd83b970a2ce0b271817d148874c
Bal Sharma <balk.sharma@gmail.com>
2019-10-23 19:30:08 -0400

    Adding digital watch to the top right corner

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
   document.onkeydown = checkKey;
 });
@@ -12,22 +12,41 @@ const checkKey = (e) => {
   }
 }
 
-  /*
-      _.-""""`-._ 
-    ,' _-""""`-_ `.
-    / ,'.-'"""`-.`. \
-  | / / ,'"""`. \ \ |
-  | | | | ,'"`. | | | |
-  | | | | |   | | | | |
+/*
+    _.-""""`-._ 
+  ,' _-""""`-_ `.
+  / ,'.-'"""`-.`. \
+| / / ,'"""`. \ \ |
+| | | | ,'"`. | | | |
+| | | | |   | | | | |
 
-  RAINBOW EASTER LOG 
-  */
+RAINBOW EASTER LOG 
+*/
 
- const rainbow = () => {
+const rainbow = () => {
   let x = 1;
   let up = !1;
   setInterval(() => {
-    (75 === x || 1 === x) && (up = !up), up ? x++ : x--, console.log(`%c${`■`.repeat(x)}`, `color: hsl(${2*x}, 100%, 50%)`)
+    (75 === x || 1 === x) && (up = !up), up ? x++ : x-- , console.log(`%c${`■`.repeat(x)}`, `color: hsl(${2 * x}, 100%, 50%)`)
   }, 15);
 };
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Adding digital watch at the top right corner. It bigins from below line ----
+const date = new Date();
+const digital_clock = document.querySelector('.clock')
+const tick = () => {
+  const now = new Date();
+  const hour = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const createClockHtml = `
+  <span>${hour}</span> :
+  <span>${minutes}</span> :
+  <span>${seconds}</span> 
+  `
+  digital_clock.innerHTML = createClockHtml;
+}
+setInterval(tick, 1000);
+// digital watch code ends at this line ------------
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
